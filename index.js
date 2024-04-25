4e4ec6fc8e7405baa58cc3741f9681c3bc2926ae
Lava Ahmed <lavaahmed915@gmail.com>
2024-04-25 04:01:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+// Write your solution in this file!
+let employee = {
+    name: "Lava",
+    streetAddress: "Erbil"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let employeeObj = {...employee};
+    employeeObj[key] = value;
+    return employeeObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key ,value){
+     employee[key] = value;
+     return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    let newobj = {...employee};
+    delete newobj[key];
+    return newobj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
